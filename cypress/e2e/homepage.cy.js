e0343bd9a61299da60a974976b45d0b491789474
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-02-08 12:30:17 +0700

    Homepage - Assert by Name and Data Attribute

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -12,5 +12,15 @@ describe('Load Homepage', () => {
 
     // assertion failed example
     // cy.contains('h1', 'Todos App')
+
+    // https://on.cypress.io/contains
+    // assert homepage title using name
+    cy.contains('h1', 'todos')
+    // assert homepage title using regular expression
+    cy.contains('h1', /^todos$/)
+    // assert homepage title & element using data attribute
+    // cy.contains('[data-cy="app-title"]', 'todos')
+    // cy.get('[data-cy="app-title"]').should('have.text', 'todos')
+    cy.get('[data-test="new-todo"]').should('have.text', '')
   })
 })
