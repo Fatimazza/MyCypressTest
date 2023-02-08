33e8023a44f5e8d0e0fa35793f62c68455e015bc
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-02-08 12:34:16 +0700

    Homepage - Fix comment description

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -3,22 +3,22 @@
 
 describe('Load Homepage', () => {
   it('Load Homepage', () => {
-    // visit web page
+    // visit Homepage
     cy.visit('https://example.cypress.io/todo')
 
     // https://on.cypress.io/get
-    // assert homepage components
+    // assert Homepage elements
     cy.get('.new-todo').get('footer').should('exist')
 
     // assertion failed example
     // cy.contains('h1', 'Todos App')
 
     // https://on.cypress.io/contains
-    // assert homepage title using name
+    // assert Homepage title using name
     cy.contains('h1', 'todos')
-    // assert homepage title using regular expression
+    // assert Homepage title using regular expression
     cy.contains('h1', /^todos$/)
-    // assert homepage title & element using data attribute
+    // assert Homepage title & element using data attribute
     // cy.contains('[data-cy="app-title"]', 'todos')
     // cy.get('[data-cy="app-title"]').should('have.text', 'todos')
     cy.get('[data-test="new-todo"]').should('have.text', '')
