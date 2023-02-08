c54a9f6e77c5a4152535665f4562f141a6a774e7
Fatima Azzahro <azzahro.fatima@gmail.com>
2023-02-08 12:50:15 +0700

    Add Items - Add Steps on each Test Case

--- cypress/e2e/2-adding-items.cy.js
+++ cypress/e2e/2-adding-items.cy.js
@@ -4,10 +4,11 @@
 describe('Manage Items', () => {
 
     it('Load Homepage', () => {
-
+        cy.visit('https://example.cypress.io/todo')
+        cy.contains('h1', 'todos')
     })
 
     it('Add 2 Items', () => {
-
+        cy.visit('https://example.cypress.io/todo')
     })
 })
